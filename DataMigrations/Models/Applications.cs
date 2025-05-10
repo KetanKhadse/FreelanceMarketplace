c8fdbf1e5689ee6951df2d89b749c429343a4633
ketan <ketankhadse14@gmail.com>
2025-05-10 17:36:13 +0530

    new packages and migrationWith Fluent Midrator Added

---
+++ DataMigrations/Models/Applications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Applications
+    {
+        [Key]
+        public string applicationId {  get; set; }
+        [Required]
+        public string applicantName { get; set; }
+        [Required]
+        public string applicationFile {  get; set; }
+
+    }
+}
