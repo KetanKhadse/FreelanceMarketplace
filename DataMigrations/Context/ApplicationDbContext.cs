cb7041f518453736a001d28075450d54c01b576a
ketan <ketankhadse14@gmail.com>
2025-05-11 01:02:02 +0530

    migration File Changes and Added New Connection with DbContext File

---
+++ DataMigrations/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> users { get; set; }  
+        public DbSet<Jobs> jobs { get; set; }
+
+
+    }
+}
