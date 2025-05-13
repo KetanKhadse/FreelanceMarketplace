9d8dc9c3cdf7430be32ae9d7a2e994b267f0ad6a
ketan <ketankhadse14@gmail.com>
2025-05-13 16:44:49 +0530

    Database Migration Added

--- DataMigrations/Context/ApplicationDbContext.cs
+++ DataMigrations/Context/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Data.Context
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
+
         }
         public DbSet<User> users { get; set; }  
         public DbSet<Jobs> jobs { get; set; }
---
+++ DataMigrations/Migrations/20250513111008_InitialMigration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "jobs",
+                columns: table => new
+                {
+                    jobId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    descriPtion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    postedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    applicationId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    createdOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jobs", x => x.jobId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    userId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    userName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    userPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    userEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createdDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.userId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "jobs");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
